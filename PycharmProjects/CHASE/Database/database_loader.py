24f17dce9a65210f4eb42e4a4bf8610e5fee09d1
SmolakK <kamil.smolak@upwr.edu.pl>
2018-07-25 12:51:42 +0200

    Database interface commit

--- PycharmProjects/CHASE/Database/database_loader.py
+++ PycharmProjects/CHASE/Database/database_loader.py
@@ -37,25 +37,23 @@ def shape_clear():
 def water_load(read_path):
     """
     Loads water consumption data into the database
-    :param write_path:
-    :return:
+    :param: write_path
     """
     water_file = open(read_path,'rb')
     cur.copy_expert(
         """COPY water(time,flow,sector) FROM STDIN WITH CSV DELIMITER AS ',' 
-        ENCODING 'utf-8' QUOTE AS '"' HEADER""",
-        water_file)
+        ENCODING 'utf-8' QUOTE AS '"' HEADER""", water_file)
     water_file.close()
+
 
 def water_folder_iter(dir):
     """
     Itarates through .csv files from given path and loads water data into the database
-    :param dir:
-    :return:
+    :param: dir
     """
     for root, dirs, filenames in os.walk(dir):
         for f in filenames:
-            read_path=os.path.join(dir,f)
+            read_path=os.path.join(dir, f)
             print(read_path)
             water_load(read_path)
 
